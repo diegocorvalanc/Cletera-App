6ed004e3b203903467c673504a43ee0c5ed75b1a
Bryan <bry.robles@duocuc.cl>
2023-10-18 20:45:33 -0300

    Base Proyecto
    Login, conexion a firebase y autenticación V1

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: true,
+
+  firebaseConfig: {
+    apiKey: "AIzaSyBD-F28jPXvXHMQoFI7ITnkFE8Rt74jb0g",
+    authDomain: "cleteraapp.firebaseapp.com",
+    projectId: "cleteraapp",
+    storageBucket: "cleteraapp.appspot.com",
+    messagingSenderId: "108355578273",
+    appId: "1:108355578273:web:25b4b77cae9affdb4a03e0"
+  }
+};
