1bdebc11732a4af19e66c01af575085bac46d91f
Bryan <bry.robles@duocuc.cl>
2023-10-25 14:34:53 -0300

    V3 base
    funcionalidades.

--- src/app/pages/auth/auth.page.ts
+++ src/app/pages/auth/auth.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, inject } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { User } from 'src/app/models/user.model';
 import { FirebaseService } from 'src/app/services/firebase.service';
+import { UtilsService } from 'src/app/services/utils.service';
 
 @Component({
   selector: 'app-auth',
@@ -15,14 +16,77 @@ export class AuthPage implements OnInit {
   });
 
   firebaseSvc = inject(FirebaseService);
+  utilSvc = inject(UtilsService);
 
   ngOnInit() {}
 
-  submit() {
+  async submit() {
     if (this.form.valid) {
-      this.firebaseSvc.signIn(this.form.value as User).then(res => {
-        console.log(res);
+
+      const loading = await this.utilSvc.loading();
+      await loading.present();
+
+      this.firebaseSvc.signIn(this.form.value as User).then((res) => {
+
+        this.getUserInfo(res.user.uid);
+
+      }).catch(error => {
+        console.log(error);
+
+        this.utilSvc.presentToast({
+          message: error.message,
+          duration: 2500,
+          color: 'primary',
+          position: 'middle',
+          icon: 'alert-circle-outline'
+        })
+
+      }).finally(() => {
+        loading.dismiss();
+
       })
     }
   }
+
+
+  async getUserInfo(uid: string) {
+    if (this.form.valid) {
+
+      const loading = await this.utilSvc.loading();
+      await loading.present();
+
+      let path = `users/${uid}`;
+
+      this.firebaseSvc.getDocument(path).then((user: User) => {
+
+        this.utilSvc.saveInLocalStorage('user', user);
+        this.utilSvc.routerLink('/main/home');
+        this.form.reset();
+
+        this.utilSvc.presentToast({
+          message: `Te damos la bienvenida ${user.name}`,
+          duration: 1500,
+          color: 'primary',
+          position: 'middle',
+          icon: 'person-circle-outline'
+        })
+
+      }).catch(error => {
+        console.log(error);
+
+        this.utilSvc.presentToast({
+          message: error.message,
+          duration: 2500,
+          color: 'primary',
+          position: 'middle',
+          icon: 'alert-circle-outline'
+        })
+
+      }).finally(() => {
+        loading.dismiss();
+
+      })
+    }
+  }
+
 }
--- src/app/services/firebase.service.ts
+++ src/app/services/firebase.service.ts
@@ -1,21 +1,65 @@
 import { Injectable, inject } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
-import { getAuth, signInWithEmailAndPassword } from 'firebase/auth';
+import {
+  getAuth,
+  signInWithEmailAndPassword,
+  createUserWithEmailAndPassword,
+  updateProfile,
+  sendPasswordResetEmail,
+} from 'firebase/auth';
 import { User } from '../models/user.model';
-
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { getFirestore, setDoc, doc, getDoc } from '@angular/fire/firestore';
+import { UtilsService } from './utils.service';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class FirebaseService {
-
   auth = inject(AngularFireAuth);
+  firestore = inject(AngularFirestore);
+  utilsSvc = inject(UtilsService);
 
-//========== autenticacion ============
+  //========== autenticacion ============
+  getAuth(){
+    return getAuth();
+  }
 
-//========== acceder ======
-signIn(user: User) {
-  return signInWithEmailAndPassword(getAuth(), user.email, user.password);
-}
+  //========== acceder ======
+  signIn(user: User) {
+    return signInWithEmailAndPassword(getAuth(), user.email, user.password);
+  }
 
+  //========== Registro ======
+  signUp(user: User) {
+    return createUserWithEmailAndPassword(getAuth(), user.email, user.password);
+  }
+
+  //========== Actualizar usuario ======
+  updateUser(displayName: string) {
+    return updateProfile(getAuth().currentUser, { displayName });
+  }
+
+  //========enviar email para reestablecer clave
+  sendRecoveryEmail(email: string) {
+    return sendPasswordResetEmail(getAuth(), email);
+  }
+
+  //==========Cerrar Sesion=======
+  signOut(){
+    getAuth().signOut();
+    localStorage.removeItem('user');
+    this.utilsSvc.routerLink('/auth')
+  }
+
+  //==================Base de datos===================
+
+  setDocument(path: string, data: any) {
+    return setDoc(doc(getFirestore(), path), data);
+  }
+
+  //================Obtener Documento===========
+  async getDocument(path: string) {
+    return (await getDoc(doc(getFirestore(), path))).data();
+  }
 }
