13dc1379032afda657772d7e825af1872f75e129
DiegoCorvalan <dcorvalan94@gmail.com>
2023-11-14 23:15:23 -0300

    wip 1

--- src/app/pages/main/main.page.ts
+++ src/app/pages/main/main.page.ts
@@ -14,7 +14,6 @@ export class MainPage implements OnInit {
     { title: 'Inicio', url: '/main/home', icon: 'home-outline' },
     { title: 'Perfil', url: '/main/profile', icon: 'person-outline' },
     { title: 'Nosotros', url: '/main/nosotros', icon: 'book-outline' },
-
   ];
 
   router = inject(Router);
