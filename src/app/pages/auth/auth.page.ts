6739a78b4b7a877e7a6b837ac919d53ae4d6b5ca
Bryan <bry.robles@duocuc.cl>
2023-11-15 21:48:58 -0300

    arreglos minimos

--- src/app/pages/auth/auth.page.ts
+++ src/app/pages/auth/auth.page.ts
@@ -22,71 +22,67 @@ export class AuthPage implements OnInit {
 
   async submit() {
     if (this.form.valid) {
-
       const loading = await this.utilSvc.loading();
       await loading.present();
 
-      this.firebaseSvc.signIn(this.form.value as User).then((res) => {
-
-        this.getUserInfo(res.user.uid);
-
-      }).catch(error => {
-        console.log(error);
-
-        this.utilSvc.presentToast({
-          message: error.message,
-          duration: 2500,
-          color: 'primary',
-          position: 'middle',
-          icon: 'alert-circle-outline'
+      this.firebaseSvc
+        .signIn(this.form.value as User)
+        .then((res) => {
+          this.getUserInfo(res.user.uid);
         })
+        .catch((error) => {
+          console.log(error);
 
-      }).finally(() => {
-        loading.dismiss();
-
-      })
+          this.utilSvc.presentToast({
+            message: error.message,
+            duration: 2500,
+            color: 'primary',
+            position: 'middle',
+            icon: 'alert-circle-outline',
+          });
+        })
+        .finally(() => {
+          loading.dismiss();
+        });
     }
   }
 
-
   async getUserInfo(uid: string) {
     if (this.form.valid) {
-
       const loading = await this.utilSvc.loading();
       await loading.present();
 
       let path = `users/${uid}`;
 
-      this.firebaseSvc.getDocument(path).then((user: User) => {
+      this.firebaseSvc
+        .getDocument(path)
+        .then((user: User) => {
+          this.utilSvc.saveInLocalStorage('user', user);
+          this.utilSvc.routerLink('/main/home');
+          this.form.reset();
 
-        this.utilSvc.saveInLocalStorage('user', user);
-        this.utilSvc.routerLink('/main/home');
-        this.form.reset();
-
-        this.utilSvc.presentToast({
-          message: `Te damos la bienvenida ${user.name}`,
-          duration: 1500,
-          color: 'primary',
-          position: 'middle',
-          icon: 'person-circle-outline'
+          this.utilSvc.presentToast({
+            message: `Te damos la bienvenida ${user.name}`,
+            duration: 1500,
+            color: 'primary',
+            position: 'middle',
+            icon: 'person-circle-outline',
+          });
         })
+        .catch((error) => {
+          console.log(error);
 
-      }).catch(error => {
-        console.log(error);
-
-        this.utilSvc.presentToast({
-          message: error.message,
-          duration: 2500,
-          color: 'primary',
-          position: 'middle',
-          icon: 'alert-circle-outline'
+          this.utilSvc.presentToast({
+            message: error.message,
+            duration: 2500,
+            color: 'primary',
+            position: 'middle',
+            icon: 'alert-circle-outline',
+          });
         })
-
-      }).finally(() => {
-        loading.dismiss();
-
-      })
+        .finally(() => {
+          loading.dismiss();
+        });
     }
   }
-
 }
